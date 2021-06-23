da500e9d8d65b2f6460b9c127616bfbe9b96ce12
Peter Lebiedzinski <63629309+plebbyd@users.noreply.github.com>
2021-06-22 19:17:16 -0500

    Add files via upload

---
+++ integratedF1.py
@@ -0,0 +1,13 @@
+#penalzed and non-penalized integrated F1 score calculator functions
+#From GitHub by Plebbyd: https://github.com/plebbyd/integrated-F1
+
+
+def penalized(conf, F1, lam=1.0, f=1.0):
+    pen_scores = [(conf[i+1] - conf[i]) * ((F1[i] + F1[i + 1]) / 2)**(f / (((conf[i] + conf[i+1]) / 2))**lam) for i in range(0, len(conf) - 1)]
+    overall_score = sum(pen_scores)
+    pen_nonpen_ratio = overall_score / non_penalized(conf, F1)[1]
+    return pen_scores, overall_score, pen_nonpen_ratio
+
+def non_penalized(conf, F1):
+    scores = [(conf[i+1] - conf[i]) * F1[i] for i in range(len(conf) - 1)]
+    return scores, sum(scores)
